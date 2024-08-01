8659b8e27a7cde0887e8969b7b6739d112658ff1
AriadnesCristie <ariadnesaraujoponte@outlook.com>
2024-08-01 16:13:29 -0300

    alterando pom.xml

--- src/main/java/jdev/mentoria/lojavirtual/loja_virtual/MarcaProduto.java
+++ src/main/java/jdev/mentoria/lojavirtual/loja_virtual/MarcaProduto.java
@@ -1,4 +1,27 @@
 package jdev.mentoria.lojavirtual.loja_virtual;
+import java.io.Serializable;
+import javax.persistence.*;
 
-public class MarcaProduto {
+@Entity
+@Table(name = "marca_produto")
+@SequenceGenerator(name = "seq_marca_produto", sequenceName = "seq_marca_produto" , allocationSize = 1, initialValue = 1)
+
+public class MarcaProduto implements Serializable{
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto" )
+    private Long id;
+
+    private String nomeDesc;
+
+    public Long getId(){
+        return id;
+    }
+    public void setId(Long id){
+        this.id=id;
+    }
+    public String getNomeDesc(){
+        return nomeDesc;
+    }
 }
